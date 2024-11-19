d528cbcb4d690bc83cd31598fd0fc1e8f7d5a797
Gonhub9 <ho_gok@naver.com>
2024-11-19 14:57:27 +0900

    Scanner

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        App app = new App();
+        app.run();
+
+
+
+
+
+
+
+    }
+}
+
+class App {
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+
+        // 생성
+        Scanner scanner = new Scanner(System.in);
+
+        while ( true ) {
+            System.out.print("명령 ) ");
+            String cmd = scanner.nextLine();
+
+            if cmd.equals("종료");
+            break;
+
+        }
+
+    }
+
+}
