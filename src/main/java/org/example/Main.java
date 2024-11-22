d2fddff7c5dac650bd9198dfe592b86344b5cea8
Gonhub9 <ho_gok@naver.com>
2024-11-22 11:22:54 +0900

    refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -13,83 +12,93 @@ public class Main {
 }
 
 class App {
+
+    // 인스턴스 변수
+    Scanner scanner;
+    int lastId;
+    Saying[] sayings;
+    int SayingsSize;
+
+    App() {
+        Scanner scanner = new Scanner(System.in);
+        lastId = 0;
+        Saying[] sayings = new Saying[3];
+        SayingsSize = 0;
+    }
+
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-        ArrayList<Saying> sayings = new ArrayList<>();
-
-        Scanner scanner = new Scanner(System.in);
-
-        int lastId = 1;
-
-        while ( true ) {
+        while (true) {
             System.out.print("명령 ) ");
             String cmd = scanner.nextLine();
 
             // 종료
-            if (cmd.equals("종료")) {
-                break;
+            if (cmd.equals("종료")) break;
 
-            // 등록
-            } else if (cmd.equals("등록")) {
+                // 등록
+            else if (cmd.equals("등록")) {
+                actionAdd();
 
-                System.out.print("명언 : ");
-                String content = scanner.nextLine();
-
-                System.out.print("작가 : ");
-                String author = scanner.nextLine();
-
-                // 클래스 불러오기
-                Saying saying = new Saying(lastId++, content, author);
-
-                // 리스트 추가
-                sayings.add(saying);
-
-                System.out.println(saying.id + "번 명언이 등록되었습니다.");
-
-            // 목록
+                // 목록
             } else if (cmd.equals("목록")) {
+                actionList();
 
-                // 목록에 아무 것도 없으면
-                if (sayings.isEmpty()) {
-                    System.out.println("등록된 명언이 없습니다.");
-
-                } else {
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-
-                    // 리스트 역순
-                    for (int i = sayings.size() - 1; i >= 0; i--) {
-                        Saying saying = sayings.get(i);
-                        System.out.printf("%d / %s / %s \n", saying.id, saying.content, saying.author);
-                    }
-                }
-
-            // 삭제
-            } else if (cmd.equals("삭제?id=")) {
-                sayings.remove(cmd.equals(""));
-
-
-
-
-
-            } else {
-                System.out.println("알 수 없는 명령어입니다.");
             }
+
+            scanner.close();
+        }
     }
-        scanner.close();
-}
 
-class Saying {
-    int id;
-    String content;
-    String author;
+    Saying addSaying(String content, String author) {
+        int id = ++lastId;
 
-    Saying(int id, String content, String author) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
+        Saying Saying = new Saying(id, content, author);
+
+        sayings[SayingsSize] = Saying;
+        SayingsSize++;
+
+        return Saying;
+    }
+
+    void actionAdd() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String author = scanner.nextLine();
+
+        Saying Saying = addSaying("나의 죽음을 적들에게 알리지 말라.", "이순신 장군");
+
+        System.out.println("%d번 명언이 등록되었습니다.".formatted(Saying.id));
+    }
+
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+        System.out.println("----------------------");
+
+        for (Saying Saying : sayings) {
+            if (Saying == null) break;
+            System.out.println("%d / %s / %s".formatted(Saying.id, Saying.content, Saying.author));
+
+        }
+    }
+
+
+    class Saying extends Object {
+        int id;
+        String content;
+        String author;
+
+        Saying(int id, String content, String author) {
+            this.id = id;
+            this.content = content;
+            this.author = author;
+        }
+
+        @Override
+        public String toString() {
+            return "Saying (id=%d, content=\"%s\", author=\"%s\")".formatted(id, content, author);
         }
     }
 }
-
